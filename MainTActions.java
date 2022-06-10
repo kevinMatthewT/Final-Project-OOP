62f25bb31217981971d0cd859ec85187b84a2027
kevin matthew T <91600300+kevinMatthewT@users.noreply.github.com>
2022-06-10 21:05:19 +0700

    Create MainTActions.java

---
+++ MainTActions.java
@@ -0,0 +1,22 @@
+package Turbot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class MainTActions {
+	public static void main(String[] args) throws LoginException{
+		//code for connecting to the bot address
+		JDABuilder Turbot = JDABuilder.createDefault("Token");
+		
+		//shows the visuals of bot current activity
+		Turbot.setActivity(Activity.listening("Nothing"));
+		Turbot.setStatus(OnlineStatus.IDLE);
+		Turbot.addEventListeners(new TurbotCommands());
+		Turbot.build();
+	}
+		
+	
+}
